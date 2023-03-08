9d17b0c752e59b3ce5604423576831f32424d31e
M-Rehan0 <muhammadrehan.chaudh@ucalgary.ca>
2023-03-08 09:05:22 -0700

    Code added in navbar.js

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#">&#9776;</a></li>
+                <li><span>Lotion</span></li>
+            </ul>
+        </nav>
+    );
+};
+
+
+export default Navbar;
